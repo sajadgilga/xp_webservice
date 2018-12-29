df531e86f19d75b897b8a05962ba5c3d3888b06d
sajadgilga <majidstic@yahoo.com>
2018-12-29 18:22:35 +0330

    refactored a bit

--- src/main/java/ir/sharif/vamdeh/webservices/webservices/userRecord/RecordResponse.kt
+++ src/main/java/ir/sharif/vamdeh/webservices/webservices/userRecord/RecordResponse.kt
@@ -1,24 +1,18 @@
 package ir.sharif.vamdeh.webservices.webservices.userRecord
 
-import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
 import ir.sharif.vamdeh.webservices.base.requestProcess.BaseResponse
 
-class RecordResponse: BaseResponse(){
-    @SerializedName("coins")
-    @Expose
-    var coins: Int = 0
+class RecordResponse(
+        @field: SerializedName("coins")
+        var coins: Int = 0,
 
-    @SerializedName("gem")
-    @Expose
-    var gem: Int = 0
+        @field: SerializedName("gem")
+        var gem: Int = 0,
 
-    @SerializedName("avatar")
-    @Expose
-    var avatar: Int = 0
+        @field: SerializedName("avatar")
+        var avatar: Int = 0,
 
-    @SerializedName("level")
-    @Expose
-    var level: Int = 0
-
-}
+        @field: SerializedName("level")
+        var level: Int = 0
+): BaseResponse()
